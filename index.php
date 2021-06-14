112fb338d14a877e165d14920748d00a6bc96359
Jan Dvorak <dvorakh1997@gmail.com>
2021-06-14 14:07:53 +0200

    index php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    <title>Můj první PHP script</title>
+    <meta charset="UTF-8">
+    </head>
+    <body>
+
+    <?php
+	$a = 10;
+	$b = 20;
+	echo "$a + $b = ". $a+$b;
+	
+    ?>
+
+    </body>
+</html>
