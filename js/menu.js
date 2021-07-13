7ac2b22096ac89c7bbc46f870e1f0e42f0bcec1d
Jan Dvorak <dvorakh1997@gmail.com>
2021-07-13 20:25:37 +0200

    kalkulačka ->cena

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var menuString = `<ul class="nav">
    <a href="index.html"><li class="nav-item">Domů</li></a>
-   <a href="sats.html"><li class="nav-item">Kalkulačka</li></a>
+   <a href="sats.html"><li class="nav-item">Cena BTC</li></a>
    <a href="o_projektu.html"> <li class="nav-item-right">O projektu</li></a>
   </ul>`
   $("#navigation").html(menuString)
