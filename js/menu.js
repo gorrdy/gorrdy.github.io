e323aabba9e0de44f0dd455a67dae20dd2c16609
Jan Dvorak <dvorakh1997@gmail.com>
2021-07-19 11:55:50 +0200

    apps menu

--- js/menu.js
+++ js/menu.js
@@ -3,9 +3,7 @@ $(document).ready(function(){
    <a href="index.html"><li class="nav-item">Domů</li></a>
    <a href="sats.html"><li class="nav-item">Cena BTC</li></a>
    <a href="pay_sats.html"><li class="nav-item">SatsPay</li></a>
-   <a target="_blank" href="https://mempool.sats.cz"><li class="nav-item">Mempool</li></a>
-   <a target="_blank" href="https://pay.sats.cz"><li class="nav-item">LNBits</li></a>
-   <a target="_blank" href="https://explorer.sats.cz"><li class="nav-item">Explorer</li></a>
+   <a href="apps.html"> <li class="nav-item">Aplikace</li></a>
    <a href="o_projektu.html"> <li class="nav-item-right">O projektu</li></a>
   </ul>`
   $("#navigation").html(menuString)
