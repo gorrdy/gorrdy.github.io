72c47a2cc8f73132ce94b60ea6d5b71db89635cd
Jan Dvořák <80678558+gorrdy@users.noreply.github.com>
2021-08-24 12:59:25 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   var menuString = `<ul class="nav">
    <a href="index.html"><li class="nav-item">Domů</li></a>
    <a href="sats.html"><li class="nav-item">Cena BTC</li></a>
+   <a href="pay_sats.html"><li class="nav-item">SatsPay</li></a>
    <a target="_blank" href="https://btcpay.sats.cz"><li class="nav-item">Eshop</li></a>
    <a href="o_projektu.html"> <li class="nav-item-right">O projektu</li></a>
    <a href="apps.html"> <li class="nav-item-right">Aplikace</li></a>
