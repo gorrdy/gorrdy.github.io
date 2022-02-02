1a27aa85526a84f67cc37982f726663c8695eef9
Jan Dvořák <80678558+gorrdy@users.noreply.github.com>
2022-02-02 17:19:42 +0100

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -36,9 +36,11 @@ $(document).ready(function(){
     <input type="range" class="btcpay-input-range" min="10" max="1000" step="10" value="10" style="width:146px;margin-bottom:15px;" oninput="handleSliderInput(event);return false" />
   </div>
 <a href="#">
-<button type="submit" class="submit" name="submit" style="min-width:146px;min-height:40px;border-radius:4px;border-style:none;background-color:#0f3b21;" title="Zaplatit pres BTCPayServer"></a><span style="color:#fff">Podpořit</span>
+<button type="submit" class="submit" name="submit" style="min-width:146px;min-height:40px;border-radius:4px;border-style:none;background-color:#0f3b21;" title="Zaplatit pres BTCPayServer">
+<span style="color:#fff">Podpořit</span>
+
 <img src="https://btcplatby.cz/wp-content/uploads/2022/01/bp_logo_01.png" style="height:40px;display:inline-block;padding:5% 0 5% 5px;vertical-align:middle;">
-</button></form>
+</button></a></form>
 <script>
     if (!window.btcpay) {
         var script = document.createElement('script');
